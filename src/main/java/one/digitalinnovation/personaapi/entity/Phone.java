8eace9ac5e438cf49c6288942ddf025c211ec5b9
CarlosEduardoDeveloper <eduardolcmdeveloper@gmail.com>
2022-01-09 12:06:01 -0300

    criacao das classes entitys e enums

---
+++ src/main/java/one/digitalinnovation/personaapi/entity/Phone.java
@@ -0,0 +1,28 @@
+package one.digitalinnovation.personaapi.entity;
+
+
+import lombok.*;
+import one.digitalinnovation.personaapi.enumns.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+
+
+}
