311f5f5d6b109f52b31621c1be676a37849c94a4
CarlosEduardoDeveloper <eduardolcmdeveloper@gmail.com>
2022-01-10 13:44:44 -0300

    criacao das classes test

---
+++ src/test/java/one/digitalinnovation/personaapi/mapper/PersonMapperTest.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.personaapi.mapper;
+
+import one.digitalinnovation.personaapi.dto.request.PersonDTO;
+import one.digitalinnovation.personaapi.dto.request.PhoneDTO;
+import one.digitalinnovation.personaapi.entity.Person;
+import one.digitalinnovation.personaapi.entity.Phone;
+import one.digitalinnovation.personaapi.util.PersonUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class PersonMapperTest {
+
+    @Autowired
+    private PersonMapper personMapper;
+
+    @Test
+    void testGivenPersonDTOThenReturnPersonEntity() {
+        PersonDTO personDTO = PersonUtil.createFakeDTO();
+        Person person = personMapper.toModel(personDTO);
+
+        assertEquals(personDTO.getFirstName(), person.getFirstName());
+        assertEquals(personDTO.getLastName(), person.getLastName());
+        assertEquals(personDTO.getCPF(), person.getCPF());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phoneDTO.getType(), phone.getType());
+        assertEquals(phoneDTO.getNumber(), phone.getNumber());
+    }
+
+    @Test
+    void testGivenPersonEntityThenReturnPersonDTO() {
+        Person person = PersonUtil.createFakeEntity();
+        PersonDTO personDTO = personMapper.toDTO(person);
+
+        assertEquals(person.getFirstName(), personDTO.getFirstName());
+        assertEquals(person.getLastName(), personDTO.getLastName());
+        assertEquals(person.getCPF(), personDTO.getCPF());
+
+        Phone phone = person.getPhones().get(0);
+        PhoneDTO phoneDTO = personDTO.getPhones().get(0);
+
+        assertEquals(phone.getType(), phoneDTO.getType());
+        assertEquals(phone.getNumber(), phoneDTO.getNumber());
+    }
+}
---
+++ src/test/java/one/digitalinnovation/personaapi/service/PersonServiceTest.java
@@ -0,0 +1,51 @@
+package one.digitalinnovation.personaapi.service;
+
+import one.digitalinnovation.personaapi.dto.request.PersonDTO;
+import one.digitalinnovation.personaapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personaapi.entity.Person;
+import one.digitalinnovation.personaapi.mapper.PersonMapper;
+import one.digitalinnovation.personaapi.repository.PersonRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static one.digitalinnovation.personaapi.util.PersonUtil.createFakeDTO;
+import static one.digitalinnovation.personaapi.util.PersonUtil.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personMapper.toModel(personDTO)).thenReturn(expectedSavedPerson);
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Person successfully created with ID " + savedPersonId)
+                .build();
+    }
+}
